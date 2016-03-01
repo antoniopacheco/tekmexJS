58251e35a8f3a1b4690509c84a7d414a5ae418eb
Antonio Pacheco <antonio_pacheco@outlook.com>
2016-03-01 14:35:18 -0800

    US date to france and hollidays

---
+++ france_hollidays.js
@@ -0,0 +1,81 @@
+var France_hollidays = {  
+   "holidays":{  
+      "2016-01-01":[  
+         {  
+            "name":"Jour de l'an",
+            "country":"FR",
+            "date":"2016-01-01"
+         }
+      ],
+      "2016-03-28":[  
+         {  
+            "name":"Lundi de Pâques",
+            "country":"FR",
+            "date":"2016-03-28"
+         }
+      ],
+      "2016-05-01":[  
+         {  
+            "name":"Fête du Travail",
+            "country":"FR",
+            "date":"2016-05-01"
+         }
+      ],
+      "2016-05-05":[  
+         {  
+            "name":"Jeudi de l'Ascension",
+            "country":"FR",
+            "date":"2016-05-05"
+         }
+      ],
+      "2016-05-08":[  
+         {  
+            "name":"Fête de la Victoire 1945",
+            "country":"FR",
+            "date":"2016-05-08"
+         }
+      ],
+      "2016-05-16":[  
+         {  
+            "name":"Lundi de Pentecôte",
+            "country":"FR",
+            "date":"2016-05-16"
+         }
+      ],
+      "2016-07-14":[  
+         {  
+            "name":"Fête nationale",
+            "country":"FR",
+            "date":"2016-07-14"
+         }
+      ],
+      "2016-08-15":[  
+         {  
+            "name":"Assomption",
+            "country":"FR",
+            "date":"2016-08-15"
+         }
+      ],
+      "2016-11-01":[  
+         {  
+            "name":"Toussaint",
+            "country":"FR",
+            "date":"2016-11-01"
+         }
+      ],
+      "2016-11-11":[  
+         {  
+            "name":"Armistice de 1918",
+            "country":"FR",
+            "date":"2016-11-11"
+         }
+      ],
+      "2016-12-25":[  
+         {  
+            "name":"Noël",
+            "country":"FR",
+            "date":"2016-12-25"
+         }
+      ]
+   }
+}
---
+++ us_hollidays.js
@@ -0,0 +1,353 @@
+var US_hollidays = {  
+   "holidays":{  
+      "2016-01-01":[  
+         {  
+            "name":"Last Day of Kwanzaa",
+            "country":"US",
+            "date":"2016-01-01"
+         },
+         {  
+            "name":"New Year's Day",
+            "country":"US",
+            "date":"2016-01-01"
+         }
+      ],
+      "2016-01-06":[  
+         {  
+            "name":"Epiphany",
+            "country":"US",
+            "date":"2016-01-06"
+         }
+      ],
+      "2016-01-07":[  
+         {  
+            "name":"Orthodox Christmas",
+            "country":"US",
+            "date":"2016-01-07"
+         }
+      ],
+      "2016-01-18":[  
+         {  
+            "name":"Martin Luther King, Jr. Day",
+            "country":"US",
+            "date":"2016-01-18"
+         }
+      ],
+      "2016-02-02":[  
+         {  
+            "name":"Groundhog Day",
+            "country":"US",
+            "date":"2016-02-02"
+         }
+      ],
+      "2016-02-10":[  
+         {  
+            "name":"Ash Wednesday",
+            "country":"US",
+            "date":"2016-02-10"
+         }
+      ],
+      "2016-02-14":[  
+         {  
+            "name":"Valentine's Day",
+            "country":"US",
+            "date":"2016-02-14"
+         }
+      ],
+      "2016-02-15":[  
+         {  
+            "name":"Washington's Birthday",
+            "country":"US",
+            "date":"2016-02-15"
+         }
+      ],
+      "2016-03-08":[  
+         {  
+            "name":"International Women's Day",
+            "country":"US",
+            "date":"2016-03-08"
+         }
+      ],
+      "2016-03-17":[  
+         {  
+            "name":"Saint Patrick's Day",
+            "country":"US",
+            "date":"2016-03-17"
+         }
+      ],
+      "2016-03-20":[  
+         {  
+            "name":"Palm Sunday",
+            "country":"US",
+            "date":"2016-03-20"
+         }
+      ],
+      "2016-03-25":[  
+         {  
+            "name":"Good Friday",
+            "country":"US",
+            "date":"2016-03-25"
+         }
+      ],
+      "2016-03-27":[  
+         {  
+            "name":"Easter",
+            "country":"US",
+            "date":"2016-03-27"
+         }
+      ],
+      "2016-04-01":[  
+         {  
+            "name":"April Fools' Day",
+            "country":"US",
+            "date":"2016-04-01"
+         }
+      ],
+      "2016-04-22":[  
+         {  
+            "name":"Earth Day",
+            "country":"US",
+            "date":"2016-04-22"
+         }
+      ],
+      "2016-04-29":[  
+         {  
+            "name":"Arbor Day",
+            "country":"US",
+            "date":"2016-04-29"
+         }
+      ],
+      "2016-05-01":[  
+         {  
+            "name":"May Day",
+            "country":"US",
+            "date":"2016-05-01"
+         }
+      ],
+      "2016-05-04":[  
+         {  
+            "name":"Star Wars Day",
+            "country":"US",
+            "date":"2016-05-04"
+         }
+      ],
+      "2016-05-05":[  
+         {  
+            "name":"Cinco de Mayo",
+            "country":"US",
+            "date":"2016-05-05"
+         }
+      ],
+      "2016-05-08":[  
+         {  
+            "name":"Mother's Day",
+            "country":"US",
+            "date":"2016-05-08"
+         }
+      ],
+      "2016-05-30":[  
+         {  
+            "name":"Memorial Day",
+            "country":"US",
+            "date":"2016-05-30"
+         }
+      ],
+      "2016-06-14":[  
+         {  
+            "name":"Flag Day",
+            "country":"US",
+            "date":"2016-06-14"
+         }
+      ],
+      "2016-06-19":[  
+         {  
+            "name":"Father's Day",
+            "country":"US",
+            "date":"2016-06-19"
+         }
+      ],
+      "2016-06-27":[  
+         {  
+            "name":"Helen Keller Day",
+            "country":"US",
+            "date":"2016-06-27"
+         }
+      ],
+      "2016-07-04":[  
+         {  
+            "name":"Independence Day",
+            "country":"US",
+            "date":"2016-07-04"
+         }
+      ],
+      "2016-08-26":[  
+         {  
+            "name":"Women's Equality Day",
+            "country":"US",
+            "date":"2016-08-26"
+         }
+      ],
+      "2016-09-05":[  
+         {  
+            "name":"Labor Day",
+            "country":"US",
+            "date":"2016-09-05"
+         }
+      ],
+      "2016-09-11":[  
+         {  
+            "name":"Grandparent's Day",
+            "country":"US",
+            "date":"2016-09-11"
+         },
+         {  
+            "name":"Patriot Day",
+            "country":"US",
+            "date":"2016-09-11"
+         }
+      ],
+      "2016-09-17":[  
+         {  
+            "name":"Constitution Day",
+            "country":"US",
+            "date":"2016-09-17"
+         }
+      ],
+      "2016-10-06":[  
+         {  
+            "name":"German-American Day",
+            "country":"US",
+            "date":"2016-10-06"
+         }
+      ],
+      "2016-10-09":[  
+         {  
+            "name":"Leif Erkson Day",
+            "country":"US",
+            "date":"2016-10-09"
+         }
+      ],
+      "2016-10-10":[  
+         {  
+            "name":"Columbus Day",
+            "country":"US",
+            "date":"2016-10-10"
+         }
+      ],
+      "2016-10-31":[  
+         {  
+            "name":"Halloween",
+            "country":"US",
+            "date":"2016-10-31"
+         }
+      ],
+      "2016-11-08":[  
+         {  
+            "name":"Election Day",
+            "country":"US",
+            "date":"2016-11-08"
+         },
+         {  
+            "name":"Super Tuesday",
+            "country":"US",
+            "date":"2016-11-08"
+         }
+      ],
+      "2016-11-11":[  
+         {  
+            "name":"Veterans Day",
+            "country":"US",
+            "date":"2016-11-11"
+         }
+      ],
+      "2016-11-24":[  
+         {  
+            "name":"Thanksgiving Day",
+            "country":"US",
+            "date":"2016-11-24"
+         }
+      ],
+      "2016-11-25":[  
+         {  
+            "name":"Black Friday",
+            "country":"US",
+            "date":"2016-11-25"
+         }
+      ],
+      "2016-12-07":[  
+         {  
+            "name":"Pearl Harbor Remembrance Day",
+            "country":"US",
+            "date":"2016-12-07"
+         }
+      ],
+      "2016-12-08":[  
+         {  
+            "name":"Immaculate Conception of the Virgin Mary",
+            "country":"US",
+            "date":"2016-12-08"
+         }
+      ],
+      "2016-12-24":[  
+         {  
+            "name":"Christmas Eve",
+            "country":"US",
+            "date":"2016-12-24"
+         }
+      ],
+      "2016-12-25":[  
+         {  
+            "name":"Christmas",
+            "country":"US",
+            "date":"2016-12-25"
+         }
+      ],
+      "2016-12-26":[  
+         {  
+            "name":"First Day of Kwanzaa",
+            "country":"US",
+            "date":"2016-12-26"
+         }
+      ],
+      "2016-12-27":[  
+         {  
+            "name":"Second Day of Kwanzaa",
+            "country":"US",
+            "date":"2016-12-27"
+         }
+      ],
+      "2016-12-28":[  
+         {  
+            "name":"Third Day of Kwanzaa",
+            "country":"US",
+            "date":"2016-12-28"
+         }
+      ],
+      "2016-12-29":[  
+         {  
+            "name":"Fourth Day of Kwanzaa",
+            "country":"US",
+            "date":"2016-12-29"
+         }
+      ],
+      "2016-12-30":[  
+         {  
+            "name":"Fifth Day of Kwanzaa",
+            "country":"US",
+            "date":"2016-12-30"
+         }
+      ],
+      "2016-12-31":[  
+         {  
+            "name":"New Year's Eve",
+            "country":"US",
+            "date":"2016-12-31"
+         },
+         {  
+            "name":"Sixth Day of Kwanzaa",
+            "country":"US",
+            "date":"2016-12-31"
+         }
+      ]
+   }
+}
