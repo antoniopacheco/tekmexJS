37c40ee99d4289742fc0d26f40868f0f073bfc29
Antonio Pacheco <antonio_pacheco@outlook.com>
2016-03-02 15:08:24 -0800

    Events assignment

---
+++ eventHandler.js
@@ -0,0 +1,20 @@
+if(document.addEventListener){
+	this.addEvent = function(elem, type, fn){
+		elem.addEventListener(type,fn,false);
+		return fn;
+	}
+	this.removeEvent = function(elem, type, fn){
+		elem.removeEventListener(type,fn,false);
+	}
+}else if(document.attachEvent){ //IE MODEL
+	this.addEvent = function(elem, type, fn){
+		var bound = function(){
+			return fn.apply(elem,arguments);
+		};
+		elem.attachEvent("on"+type,bound);
+		return bound;
+	};
+	this.removeEvent = function(elem,type,fn){
+		elem.detachEvent("on"+type,fn);
+	}
+}
